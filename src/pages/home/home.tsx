e4ac53839d810c7b3569b5d2cd4a168ce7818afd
Ritin Malhotra <rm5486@nyu.edu>
2023-01-04 22:34:31 +0530

    Add Name + Intro Paragraph

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import './home.css';
 import TechTable from '../../components/tech-table/techTable';
+import HomeIntro from '../../components/home-intro/HomeIntro';
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>Home</h1>
-            <h3>🛠️ Tools and Technologies</h3>
+            <h1 className='home-heading'>Ritin Malhotra</h1>
+            <HomeIntro />
+            <h2>🛠️ Tools and Technologies</h2>
             <TechTable />
         </div>
     )
