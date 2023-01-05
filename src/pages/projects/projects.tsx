b928a0e6568b71dbce72178ae2e5670bc71642ed
Ritin Malhotra <rm5486@nyu.edu>
2023-01-06 01:54:31 +0530

    Change Project Card Alignment

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -4,6 +4,7 @@ import ProjectCard from '../../components/project-card/ProjectCard'
 import odinEtchASketch from '../../assets/images/odin-etch-a-sketch.png'
 import busyAgile from '../../assets/images/busy-agile.png'
 import dataMisinformation from '../../assets/images/data-misinformation.png'
+import projectPortfolio from '../../assets/images/project-portfolio.png'
 import cryptoExpert from '../../assets/images/crypto-expert.png'
 
 const Projects = () => {
@@ -14,7 +15,7 @@ const Projects = () => {
                 <ProjectCard projectName='Etch-a-Sketch' projectContributors='Individual' projectDescription='An interactive drawing tool replete with fun features.' projectImage={odinEtchASketch} projectLink='https://ritindev.github.io/odin-etch-a-sketch/' projectGithub='https://github.com/RitinDev/odin-etch-a-sketch' />
                 <ProjectCard projectName='Busy?' projectContributors='Team' projectDescription='A social calendar to share your schedules with your friends to find the best time to connect.' projectImage={busyAgile} projectLink='https://busy-8qzj4.ondigitalocean.app/login' projectGithub='https://github.com/agiledev-students-fall2022/final-project-team-busy' />
                 <ProjectCard projectName='Data Misinformation Project' projectContributors='Team' projectDescription='A data analysis project comparing correct and misleading Covid-19 related tweets.' projectImage={dataMisinformation} projectLink='https://ritindev.github.io/data-misinformation-project/' projectGithub='https://github.com/RitinDev/data-misinformation-project' />
-                {/* TODO: Add Perosnal Portfolio Page to Projects */}
+                <ProjectCard projectName='Project Portfolio' projectContributors='Individual' projectDescription='A responsive portfolio to showcase my projects and skills.' projectImage={projectPortfolio} projectLink='https://ritindev.github.io/project-portfolio/' projectGithub='https://github.com/RitinDev/vite-project-portfolio' />
                 <ProjectCard projectName='Crypto Expert' projectContributors='Team' projectDescription='Learn more about your favorite cryptocurrency tokens and predict prices with our LSTM neural network.' projectImage={cryptoExpert} projectLink='http://34.238.240.4:5000/' projectGithub='https://github.com/KrisesMaskey/Crypto_Expert'/>
             </div>
         </div>
