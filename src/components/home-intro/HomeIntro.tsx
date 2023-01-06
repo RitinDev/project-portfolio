bdd916773cb49ad71feaeec9aed511a5e62a8cae
Ritin Malhotra <rm5486@nyu.edu>
2023-01-07 02:31:30 +0530

    Navigation Fix + Adjust Home Intro Contact Link

--- src/components/home-intro/HomeIntro.tsx
+++ src/components/home-intro/HomeIntro.tsx
@@ -4,7 +4,7 @@ const HomeIntro = () => {
     return (
         <div className="home-intro">
             <h2 className='greeting'>Hello there!</h2>
-            <p> I'm Ritin, a Computer Science Major at New York University interested in full-stack web development and working with data. I'm passionate about creating intuitive and user-friendly web applications and meaningful data products. I'm looking for opportunities to apply my knowledge of software engineering, database design and data analysis in a professional environment. Feel free to reach out to me via any links provided in the <a href='/contact'>Contact</a> Page.</p>
+            <p> I'm Ritin, a Computer Science Major at New York University interested in full-stack web development and working with data. I'm passionate about creating intuitive and user-friendly web applications and meaningful data products. I'm looking for opportunities to apply my knowledge of software engineering, database design and data analysis in a professional environment. Feel free to reach out to me via any links provided in the <a href='/project-portfolio/#/contact'>Contact</a> Page.</p>
         </div>
     )
 }
