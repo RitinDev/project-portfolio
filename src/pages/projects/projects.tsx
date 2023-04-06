c08d88b376b32f5db9a60834d3b2300769464ca0
Ritin Malhotra <rm5486@nyu.edu>
2023-04-06 20:00:47 +0400

    Add Frontend Task for Emotech to Projects

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,6 +6,7 @@ import busyAgile from '../../assets/images/busy-agile.png'
 import dataMisinformation from '../../assets/images/data-misinformation.png'
 import projectPortfolio from '../../assets/images/project-portfolio.png'
 import cryptoExpert from '../../assets/images/crypto-expert.png'
+import emotechTask from '../../assets/images/emotech-task.png'
 
 const Projects = () => {
     return (
@@ -16,7 +17,8 @@ const Projects = () => {
                 <ProjectCard projectName='Busy?' projectContributors='Team' projectDescription='A social calendar to share your schedules with your friends to find the best time to connect.' projectImage={busyAgile} projectLink='https://busy-8qzj4.ondigitalocean.app/login' projectGithub='https://github.com/agiledev-students-fall2022/final-project-team-busy' />
                 <ProjectCard projectName='Data Misinformation Project' projectContributors='Team' projectDescription='A data analysis project comparing correct and misleading Covid-19 related tweets.' projectImage={dataMisinformation} projectLink='https://ritindev.github.io/data-misinformation-project/' projectGithub='https://github.com/RitinDev/data-misinformation-project' />
                 <ProjectCard projectName='Project Portfolio' projectContributors='Individual' projectDescription='A responsive portfolio to showcase my projects and skills.' projectImage={projectPortfolio} projectLink='https://ritindev.github.io/project-portfolio/' projectGithub='https://github.com/RitinDev/vite-project-portfolio' />
-                <ProjectCard projectName='Crypto Expert' projectContributors='Team' projectDescription='Learn more about your favorite cryptocurrency tokens and predict prices with our LSTM neural network.' projectImage={cryptoExpert} projectLink='http://34.238.240.4:5000/' projectGithub='https://github.com/KrisesMaskey/Crypto_Expert'/>
+                <ProjectCard projectName='Crypto Expert' projectContributors='Team' projectDescription='Learn more about your favorite cryptocurrency tokens and predict prices with our LSTM neural network.' projectImage={cryptoExpert} projectLink='http://34.238.240.4:5000/' projectGithub='https://github.com/KrisesMaskey/Crypto_Expert' />
+                <ProjectCard projectName='Emotech Internship Task' projectContributors='Individual' projectDescription='A responsive dashboard for Frontend Developer Role at Emotech' projectImage={emotechTask} projectLink='https://emotech-frontend-task.netlify.app/' projectGithub='https://github.com/RitinDev/emotech-frontend-task' />
             </div>
         </div>
     )
