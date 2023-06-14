aaf80023c9c8fe8efd3cc404419cc89255b71be4
Ritin Malhotra <rm5486@nyu.edu>
2023-06-14 08:13:34 +0530

    Introduce Changes to the Contacts Page

---
+++ src/components/contacts-lister/contactsLister.tsx
@@ -0,0 +1,68 @@
+import './contactsLister.css'
+import githubContact from '../../assets/images/contact-page/github-backdrop.jpeg'
+import linkedinContact from '../../assets/images/contact-page/linkedin-backdrop.png'
+import emailContact from '../../assets/images/contact-page/gmail-backdrop.jpeg'
+
+import { GithubFilled, LinkedinFilled, MailOutlined, InstagramOutlined } from '@ant-design/icons';
+
+const socialMediaLinks = {
+    github: 'https://github.com/RitinDev',
+    linkedin: 'https://www.linkedin.com/in/ritin-malhotra/',
+    email: '/project-portfolio/#/mail',
+    // instagram: 'https://www.instagram.com/ritinmalhotra/?hl=en'
+}
+
+const logoStyle: React.CSSProperties = {
+    fontSize: '2.75rem',
+    position: 'absolute',
+    top: '30%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+    width: '100%',
+};
+const userNameStyle: React.CSSProperties = {
+    fontSize: '1.25rem',
+    position: 'absolute',
+    top: '42.5%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white',
+    width: '100%',
+};
+
+const ContactsGrid = () => {
+    return (
+        <div className="contacts-lister">
+            <div className="contacts-lister-item mail" onClick={() => {
+                window.open(socialMediaLinks.email, '_self')
+            }
+            }>
+                <img src={emailContact} alt="email" />
+                <MailOutlined style={logoStyle} className='contact-logo' />
+                <p style={userNameStyle}>rm5486@nyu.edu</p>
+            </div>
+            <h3 className='secondary-contacts-heading'>Also, feel free to visit:</h3>
+            <div className="secondary-contacts">
+                <div className="contacts-lister-item github" onClick={() => {
+                    window.open(socialMediaLinks.github, '_blank')
+                }
+                }>
+                    <img src={githubContact} alt="github" />
+                    <GithubFilled style={logoStyle} className='contact-logo' />
+                    <p style={userNameStyle}>RitinDev</p>
+                </div>
+                <div className="contacts-lister-item linkedin" onClick={() => {
+                    window.open(socialMediaLinks.linkedin, '_blank')
+                }
+                }>
+                    <img src={linkedinContact} alt="linkedin" />
+                    <LinkedinFilled style={logoStyle} className='contact-logo' />
+                    <p style={userNameStyle}>Ritin Malhotra</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactsGrid
--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,12 +1,12 @@
 import './contact.css'
 import { GithubFilled, LinkedinFilled, MailOutlined, InstagramOutlined } from '@ant-design/icons';
-import ContactsGrid from '../../components/contacts-grid/contactsGrid';
+import ContactsGrid from '../../components/contacts-lister/contactsLister';
 
 const Contact = () => {
     return (
         <div className="contact">
             <h1 className='contact__contact-heading'>Contact</h1>
-            <h2>Reach out to me via: </h2>
+            <h2>Send me a mail!</h2>
             <div className='contact__contacts-grid'>
                 <ContactsGrid />
             </div>
