5c02054f58c6e7ea94be17e83619cf5b2cf54444
Ritin Malhotra <rm5486@nyu.edu>
2023-07-24 20:24:38 +0530

    Add Metadata Tags to `App.tsx`

---
+++ src/components/SEO/SEO.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+interface SEOProps {
+    title?: string;
+    description?: string;
+    url?: string;
+    lang?: string;
+    image?: string;
+    author?: string;
+    keywords?: string[];
+}
+
+const SEO: React.FC<SEOProps> = ({
+    title = 'Ritin Malhotra Project Portfolio',
+    description = 'Welcome to the portfolio page of Ritin Malhotra, a Computer Science Major at New York University with a keen interest in full-stack web development and working with data.',
+    url = 'https://ritindev.github.io/project-portfolio/',
+    image = 'https://ritindev.github.io/project-portfolio/vite.svg',
+    author = 'Ritin Malhotra',
+    keywords = ['Ritin Malhotra', 'project', 'portfolio', 'full-stack web development', 'data', 'NYU', 'Computer Science'],
+    lang = 'en'
+}) => {
+    return (
+        <Helmet prioritizeSeoTags>
+            <html lang={lang} />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="author" content={author} />
+            <link rel="canonical" href={url} />
+            <meta property="og:url" content={url} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+            <meta property="og:type" content="website" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={author} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={image} />
+            {keywords.length > 0 && <meta name="keywords" content={keywords.join(',')} />}
+        </Helmet>
+    );
+}
+
+export default SEO;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,28 +1,33 @@
 import './App.css'
 import Navbar from '../components/nav/Navbar'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
 
 import Home from './home/home'
 import Projects from './projects/projects'
 import Contact from './contact/contact'
 import Mail from './mail/mail'
+import SEO from '../components/SEO/SEO';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div className="app-wrapper">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/mail" element={<Mail />} />
-          </Routes>
-        </div>
+    <HelmetProvider>
+      <div className="App">
+        <Router>
+          <SEO />
+          <div className="app-wrapper">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/mail" element={<Mail />} />
+            </Routes>
+          </div>
           <Navbar />
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </HelmetProvider>
   )
 }
 
-export default App
+export default App;
