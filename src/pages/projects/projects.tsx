9964fcd1bef26b7cc47fcaf39157eec28085b698
Ritin Malhotra <rm5486@nyu.edu>
2024-01-31 05:12:43 +0400

    feat: improve navbar + projects page header styling

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -12,7 +12,7 @@ import toDoTogether from '../../assets/images/to-do-together.png'
 const Projects = () => {
     return (
         <div className="projects">
-            <h1>Projects</h1>
+            <h1 className='heading'>Projects</h1>
             <div className="projects__container">
                 <ProjectCard projectName='Etch-a-Sketch' projectContributors='Individual' projectDescription='An interactive drawing tool replete with fun features.' projectImage={odinEtchASketch} projectLink='https://ritindev.github.io/odin-etch-a-sketch/' projectGithub='https://github.com/RitinDev/odin-etch-a-sketch' />
                 <ProjectCard projectName='Busy?' projectContributors='Team' projectDescription='A social calendar to share your schedules with your friends to find the best time to connect.' projectImage={busyAgile} projectLink='https://busy-8qzj4.ondigitalocean.app/login' projectGithub='https://github.com/agiledev-students-fall2022/final-project-team-busy' />
